a08d97516633b626744c2700ed0d419df81901c8
Mike Daley <michaelcdaley@gmail.com>
2015-04-15 08:38:08 -0700

    Fixed bug to return the string instead of writing string to stdout

--- lib/budman.rb
+++ lib/budman.rb
@@ -6,14 +6,14 @@ module Budman
   # Drink budweiser
   #
   def self.drink_beer
-    puts "Budman says drink Budweiser, dude"
+    "Budman says drink Budweiser, dude"
   end
   
   ##
   # Budman's opinion about IPAs
   #
   def self.ipa
-    puts "Budman does not like IPAs - too hoppy"
+    "Budman does not like IPAs - too hoppy"
   end
 
 end # end of module Budman
